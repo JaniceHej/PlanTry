f7ff2b5a70949307cc3619d077d4d0a786b83f6b
sathwika05 <sathwikaparshaboina05@gmail.com>
2021-09-09 23:13:58 -0400

    Added the Splash Screen with Animations.

--- app/src/main/java/com/example/plantry/MainActivity.java
+++ app/src/main/java/com/example/plantry/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.plantry;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,25 +20,31 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.plantry.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-
+     private static int SPLASH_SCREEN=5000;
+    //Variables
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        //Animations
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //Hooks
+        image=findViewById(R.id.imageView);
+        image.setAnimation(topAnim);
+        image.setAnimation(bottomAnim);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
-
 }
