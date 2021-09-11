72aac22bd5db72a781c164d21a4d56ca01439231
sathwika05 <sathwikaparshaboina05@gmail.com>
2021-09-11 18:20:05 -0400

    Added the Login and SignUp pages

---
+++ app/src/main/java/com/example/plantry/Login.java
@@ -0,0 +1,28 @@
+package com.example.plantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity  {
+    Button callSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+        callSignUp=findViewById(R.id.signup_screen);
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
